87625fa6f0e0857cc569dd5f68165f57104e3965
Gyanendra Mishra <anomaly.the@gmail.com>
2015-09-06 00:24:13 +0530

    Started working on SEPT15/LIGHTHSE

---
+++ lighthouse.cpp
@@ -0,0 +1,34 @@
+//in progress
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <string.h>
+#include <cstdlib>
+#include <limits.h>
+#include <tr1/unordered_map>
+#include <tr1/unordered_set>
+#include <functional>
+#include <stack>
+#include <queue>
+using namespace std;
+typedef long long int lli;
+typedef unsigned int ui;
+#define max(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
+#define min(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _b : _a; })
+int main(){
+    lli t;
+    cin>>t;
+    while(t--){
+        lli n, i=0;
+        cin>>n;
+        lli coordinates[2][n];
+        while(i!=n){
+            scanf("%lld %lld",&coordinates[0][i],&coordinates[1][i]);
+        }
+    }
+}
